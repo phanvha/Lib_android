e16e21e3f3ffe2cf174f138958cc19eb16475b55
phanvha <17it137@sict.udn.vn>
2020-03-17 14:35:10 +0700

    đã lưu thfành công, đang viết hàm đọc dữ liệu từ sqlite

--- smartcodeslib/src/main/java/com/map4d/smartcodeslib/SmartCodeLib.java
+++ smartcodeslib/src/main/java/com/map4d/smartcodeslib/SmartCodeLib.java
@@ -25,7 +25,7 @@ import retrofit2.Response;
 
 public class SmartCodeLib {
     public static Context context;
-    public static String json;
+    public static String json, code;
     public static int count;
     public static Model_Smartcode_Data model_smartcode_data;
     public static List<Model_vmapCode_Json> model_vmapCode_jsons = new ArrayList<>();
@@ -210,37 +210,41 @@ public class SmartCodeLib {
         }
         return count;
     }
-    public static JSONArray getAllDataFromSQLite(Context context) {
+    public static String getAllDataFromSQLite(Context context) {
 
         try {
             db = SQLite.getInstance(context);
             jsonObject = new JSONObject();
             jsonArray = new JSONArray();
             model_vmapCode_jsons = new ArrayList<>();
-            model_vmapCode_jsons = db.getAll();
-            for (int i = model_vmapCode_jsons.size() - 1;i>=0;--i){
-                JSONObject jsonObject = new JSONObject();
-                jsonObject.put("id", model_vmapCode_jsons.get(i).getId());
-                jsonObject.put("address", model_vmapCode_jsons.get(i).getAddress());
-                jsonObject.put("code", model_vmapCode_jsons.get(i).getAddress());
-                jsonObject.put("doiTuongGanMa", model_vmapCode_jsons.get(i).getDoiTuongGanMa());
-                jsonObject.put("isDeleted", model_vmapCode_jsons.get(i).getDeleted());
-                jsonObject.put("latitude", model_vmapCode_jsons.get(i).getLatitude());
-                jsonObject.put("longitude", model_vmapCode_jsons.get(i).getLongitude());
-                jsonObject.put("maBuuChinh", model_vmapCode_jsons.get(i).getMaBuuChinh());
-                jsonObject.put("maHuyen", model_vmapCode_jsons.get(i).getMaHuyen());
-                jsonObject.put("maTinh", model_vmapCode_jsons.get(i).getMaTinh());
-                jsonObject.put("tenHuyen", model_vmapCode_jsons.get(i).getTenHuyen());
-                jsonObject.put("tenTinh", model_vmapCode_jsons.get(i).getTenTinh());
-                jsonArray.put(jsonObject);
-                Log.e("jsonArray", jsonArray.toString());
+            if (db.getCountTotalListVmapCodeTB()!=0){
+                model_vmapCode_jsons = db.getAll();
+                code = model_vmapCode_jsons.get(1).getCode();
+//                for (int i = model_vmapCode_jsons.size() - 1;i>=0;--i){
+//                    JSONObject jsonObject = new JSONObject();
+//                    jsonObject.put("id", model_vmapCode_jsons.get(i).getId());
+//                    jsonObject.put("address", model_vmapCode_jsons.get(i).getAddress());
+//                    jsonObject.put("code", model_vmapCode_jsons.get(i).getAddress());
+//                    jsonObject.put("doiTuongGanMa", model_vmapCode_jsons.get(i).getDoiTuongGanMa());
+//                    jsonObject.put("isDeleted", model_vmapCode_jsons.get(i).getDeleted());
+//                    jsonObject.put("latitude", model_vmapCode_jsons.get(i).getLatitude());
+//                    jsonObject.put("longitude", model_vmapCode_jsons.get(i).getLongitude());
+//                    jsonObject.put("maBuuChinh", model_vmapCode_jsons.get(i).getMaBuuChinh());
+//                    jsonObject.put("maHuyen", model_vmapCode_jsons.get(i).getMaHuyen());
+//                    jsonObject.put("maTinh", model_vmapCode_jsons.get(i).getMaTinh());
+//                    jsonObject.put("tenHuyen", model_vmapCode_jsons.get(i).getTenHuyen());
+//                    jsonObject.put("tenTinh", model_vmapCode_jsons.get(i).getTenTinh());
+//                    jsonArray.put(jsonObject);
+//                    Log.e("jsonArray", jsonArray.toString());
+//                }
             }
+
         }catch (Exception e){
             e.printStackTrace();
         }
-        Log.e("jsonArray", jsonArray.toString());
+        //Log.e("jsonArray", jsonArray.toString());
 
-        return jsonArray;
+        return code;
     }
     public static Boolean checkData(Context context){
         boolean count = false;
